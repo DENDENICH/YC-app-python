4923254354bbeb09be64758c002e1a21693fc76d
DENDENICH <daniil.gol26@yandex.ru>
2025-11-24 14:35:55 +0300

    update host

--- app/main.py
+++ app/main.py
@@ -150,5 +150,5 @@ def delete_user(user_id: int):
 
 # Запуск приложения
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
     
